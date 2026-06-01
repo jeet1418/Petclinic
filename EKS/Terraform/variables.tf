2574202e42815c13eb1f73d28852fd3d23b37d58
satyajit <satyajit.1418@gmail.com>
2026-06-01 11:12:07 +0530

    fix: Cluster version left empty

--- EKS/Terraform/variables.tf
+++ EKS/Terraform/variables.tf
@@ -37,7 +37,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.30"
+  default     = ""
 }
 
 variable "node_groups" {
